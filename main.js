c0d0f32693c0a7528bc8a794d5390061ac2e31f8
Rud3us <rodionpylnev@yandex.ru>
2026-03-18 10:39:42 +0300

    Step 5: click event handling

--- main.js
+++ main.js
@@ -9,3 +9,9 @@ title.style.padding = '10px'
 text.textContent = 'This is a simple example of DOM manipulation.'
 text.style.fontSize = '18px'
 text.style.color = 'green'
+const btn = document.getElementById('btn')
+btn.addEventListener('click', () => {
+	alert('Button clicked!')
+	btn.style.backgroundColor = 'red'
+	btn.style.color = 'white'
+})
