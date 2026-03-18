498883c47603eab6c40b7cb48e99b84453a183f6
Rud3us <rodionpylnev@yandex.ru>
2026-03-18 10:35:55 +0300

    Step 4: modifying DOM content and styles

--- main.js
+++ main.js
@@ -2,3 +2,10 @@ const title = document.getElementById('title')
 const text = document.querySelector('.text')
 console.log(title)
 console.log(text)
+title.textContent = 'Hello World'
+title.style.color = 'blue'
+title.style.backgroundColor = 'white'
+title.style.padding = '10px'
+text.textContent = 'This is a simple example of DOM manipulation.'
+text.style.fontSize = '18px'
+text.style.color = 'green'
